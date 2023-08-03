46591f52f9c4e420beba06a82d13671794d01973
John Matthew Lubi <138768200+winraaawr@users.noreply.github.com>
2023-08-03 17:00:46 +0800

    Update projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="projects" class="container flex flex-col py-8 my-8 h-auto md:min-h-[60vh] w-full">
+    <div id="projects" class="container flex flex-col py-16 my-16 h-auto md:min-h-[60vh] w-full">
         <h1 class="text-display font-bold text-3xl text-white">Projects</h1>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 my-2">
 
@@ -7,22 +7,22 @@
                 <span class="flex flex-row items-center justify-center mb-4 h-48 w-full bg-gray-800 rounded-md">
                     <img src="../assets/at-front-logo.png" class="h-24 w-auto object-cover object-center" alt="AniFox Logo">
                 </span>
-                <h2 class="text-display font-bold text-xl">at-Front</h2>
-                <h3 class="text-display text-rose-500 mb-4">Personal Project</h3>
+                <h2 class="text-display font-bold text-lg">at-Front</h2>
+                <h3 class="text-display text-rose-500 text-base mb-4">Personal Project</h3>
                 <div class="flex flex-wrap mb-4">
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">Spotify API</span>
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">Javascript</span>
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">Vue 3</span>
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">Tailwindcss</span>
                 </div>
-                <h6 class="text-body mb-2">Description</h6>
-                <span class="mb-6 md:mr-8 text-body text-sm md:text-base">
-                    A collection of front-end landing pages design.
+                <h6 class="text-body text-sm mb-2">Description</h6>
+                <span class="mb-6 md:mr-8 text-body leading-6 text-sm">
+                    A collection of front-end landing pages design where I showcase my ability and skill to develop responsive web pages.
                     <br>
                 </span>
                 <div class="flex">
-                    <a class="text-display bg-rose-500 py-2 px-3 text-sm rounded-md hover:bg-rose-600 hover:shadow-md transition-all ease-in-out"
-                        href="https://at-front.pages.dev/">View Project</a>
+                    <a class="text-display bg-rose-500 py-2 px-3 text-xs rounded-md hover:bg-rose-600 hover:shadow-md transition-all ease-in-out"
+                        href="https://at-front.pages.dev/" target="_blank">View Project</a>
                 </div>
             </div>
 
@@ -30,7 +30,7 @@
                 <span class="flex flex-row items-center justify-center mb-4 h-48 w-full bg-gray-800 rounded-md">
                     <img src="../assets/anifox-logo.png" class="h-24 w-auto object-cover object-center" alt="AniFox Logo">
                 </span>
-                <h2 class="text-display font-bold text-xl">AniFox</h2>
+                <h2 class="text-display font-bold text-lg">AniFox</h2>
                 <h3 class="text-display text-rose-500 mb-4">Personal Project</h3>
                 <div class="flex flex-wrap mb-4">
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">Jikan API</span>
@@ -38,14 +38,14 @@
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">Vue 3</span>
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">Tailwindcss</span>
                 </div>
-                <h6 class="text-body mb-2">Description</h6>
-                <span class="mb-6 md:mr-8 text-body text-sm md:text-base">
-                    An anime discovery web application powered by Jikan API, MyAnimeList's unofficial open-source API.
+                <h6 class="text-body text-sm mb-2">Description</h6>
+                <span class="mb-6 md:mr-8 text-body leading-6 text-sm">
+                    AniFox is a website that made it easy for people who love anime to find new shows. It used a tool called the Jikan API, which collects data from a website called MyAnimeList.net. The website was designed to be easy to use. People could easily search for lots of different anime shows and find ones they might like. It also showed the best and most popular shows on its main page to help users discover new favorites.
                     <br>
                 </span>
                 <div class="flex">
-                    <a class="text-display bg-rose-500 py-2 px-3 text-sm rounded-md hover:bg-rose-600 hover:shadow-md transition-all ease-in-out"
-                        href="https://winraaawr.github.io/anifox/">View Project</a>
+                    <a class="text-display bg-rose-500 py-2 px-3 text-xs rounded-md hover:bg-rose-600 hover:shadow-md transition-all ease-in-out"
+                        href="https://winraaawr.github.io/anifox/" target_blank>View Project</a>
                 </div>
             </div>
 
@@ -53,8 +53,8 @@
                 <span class="flex flex-row items-center justify-center mb-4 h-48 w-full bg-pink-400 rounded-md">
                     <img src="../assets/cakey-logo.png" class="h-36 w-auto object-cover object-center" alt="cakey logo">
                 </span>
-                <h2 class="text-display font-bold text-xl">Cakey: An Online Cake Ordering System</h2>
-                <h3 class="text-display text-rose-500 mb-4">Capstone Project</h3>
+                <h2 class="text-display font-bold text-lg line-clamp-1">Cakey: An Online Cake Ordering System</h2>
+                <h3 class="text-display text-rose-500 text-base mb-4">Capstone Project</h3>
                 <div class="flex flex-wrap mb-4">
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">PHP</span>
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">Javascript</span>
@@ -62,8 +62,8 @@
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">Bootstrap</span>
                     <span class="text-xs border-2 border-rose-500 rounded-md mr-2 mb-2 px-2 py-1">MariaDB</span>
                 </div>
-                <h6 class="text-body mb-2">Description</h6>
-                <span class="mb-6 md:mr-8 text-body text-sm md:text-base line-clamp-3">
+                <h6 class="text-body text-sm mb-2">Description</h6>
+                <span class="mb-6 md:mr-8 text-body leading-6 text-sm">
                     Cakey is an Online Cake Ordering System
                     which was developed as Capstone Project in fulfillment of the
                     requirements for the degree Bachelor of Science in Information Technology in
@@ -76,7 +76,7 @@
                     Cakey is the Key to Custom Cakes!
                 </span>
                 <div class="flex">
-                    <a class="text-display bg-rose-500 py-2 px-3 text-sm rounded-md hover:bg-rose-600 hover:shadow-md transition-all ease-in-out"
+                    <a class="text-display bg-rose-500 py-2 px-3 text-xs rounded-md hover:bg-rose-600 hover:shadow-md transition-all ease-in-out"
                         href="https://cakey.x10.mx" target="_blank">View Project</a>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "20px",
+      padding: "10px",
     },
     extend: {
       fontFamily: {
